1b8c5945c1ebd5dfde771dea138c718b5c734f9c
FVenegas93 <vennegoor2493@gmail.com>
2025-06-28 17:56:55 +0200

    Added content to Version notes page

--- src/components/conf/VersionNotes.js
+++ src/components/conf/VersionNotes.js
@@ -15,7 +15,8 @@ const VersionNotes = () => {
     
                 <ul className='my-3'>
                     <li className='li-text'>Creación de la página Estadísticas, donde el usuario podrá ver quién es pichichi y líder de asistencias.</li>
-                    <li className='li-text'></li>
+                    <li className='li-text'>Cambio en la lógica de partidos para que separe los goles marcados en los 90 minutos y, en caso de empate, que los actualice en la prórroga.</li>
+                    <li className='li-text'>Adición de los 4 puntos al ganador de la fase de grupos, que ha pasado de tener 48 puntos a 52.</li>
                 </ul>
             </main>
         </div>
